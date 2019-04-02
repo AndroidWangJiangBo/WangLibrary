038ddf9d295ef7fb1a574b57ccac96aa5a244c5c
王姜博 <1171750219@qq.com>
2019-04-02 10:12:04 +0800

    second

--- app/src/main/java/com/android/wangjj/bean/EventCode.java
+++
@@ -1,5 +0,0 @@
-package com.android.wangjj.bean;
-
-public class EventCode {
-    public static final int EVENT_A = 1000;//eg
-}
--- app/src/main/java/com/android/wangjj/net/RetrofitService.java
+++
@@ -1,7 +0,0 @@
-package com.android.wangjj.net;
-
-public interface RetrofitService {
-    //eg
-//    @POST("pub_v2/app/app_pay.php")
-//    Observable<WXResult> getWXResult();
-}
